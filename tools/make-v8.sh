661ed79371958b7e1c7f05c18ec79d4cd893f5bc
rickyes <0x19951125@gmail.com>
2020-10-16 17:53:45 +0800

    chore: add depot_tools

--- tools/make-v8.sh
+++ tools/make-v8.sh
@@ -7,12 +7,12 @@ cd deps/v8
 
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git _depot_tools
 
-gclient=$PWD/_depot_tools/gclient
+TOOL_PATH=$PWD/_depot_tools
 
-cd _depot_tools && gclient && cd ..
+cd _depot_tools && $TOOL_PATH/gclient && cd ..
 
-gclient sync
+$TOOL_PATH/gclient sync
 
 tools/dev/v8gen.py x64.release.sample 
 # PATH=~/_depot_tools:$PATH gn gen -v out.gn/$BUILD_ARCH_TYPE --args="is_component_build=false is_debug=false use_goma=false goma_dir=\"None\" use_custom_libcxx=false v8_target_cpu=\"$TARGET_ARCH\" target_cpu=\"$TARGET_ARCH\" v8_enable_backtrace=true"
-ninja -C out.gn/$BUILD_ARCH_TYPE v8_monolith
+$TOOL_PATH/ninja -C out.gn/$BUILD_ARCH_TYPE v8_monolith
